8af2ca1e7867ea073c49bd39fc55eed9cd8e140e
Alex Guzey <alex.guzey@gmail.com>
2026-01-07 12:03:50 +0100

    feat(eslint-plugin): Removed tests placeholder

--- packages/eslint-plugin/src/index.test.ts
+++
@@ -1,9 +0,0 @@
-import { describe, it, expect } from 'vitest';
-
-describe('Placeholder', () => {
-  it('should pass until eslint-plugin is implemented', () => {
-    // Placeholder test to make CI pass
-    // This package will be implemented in Task 3-4
-    expect(true).toBe(true);
-  });
-});
